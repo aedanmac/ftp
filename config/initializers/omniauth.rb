ec45d68e51a1c8b5470df9a3e593bc8174762fa3
Roy Gardiner Jr <roygardinerjr@gmail.com>
2015-06-16 19:47:50 -0700

    Oauth deploy attempt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
-google_client_id = Figaro.env.google_client_id
-google_client_secret = Figaro.env.google_client_secret
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, google_client_id, google_client_secret
+	$google_client_id = Figaro.env.google_client_id
+	$google_client_secret = Figaro.env.google_client_secret
+
+    provider :google_oauth2, $google_client_id, $google_client_secret
 end
