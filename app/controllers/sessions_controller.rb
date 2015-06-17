7bc90e3d6a430d714dde8289e2431b7a57888273
Roy Gardiner Jr <roygardinerjr@gmail.com>
2015-06-17 15:37:58 -0700

    bug fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to new_session_path
+    if user
+      session[:user_id] = user.id
+      redirect_to new_session_path
+    else
+      redirect_to new_session_path
+    end
   end
 
   def destroy
